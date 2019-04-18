b2335983e4f25b1609541fbacc0b23d420a185d9
Costin Sprincenatu <csprincenatu@axway.com>
2019-04-18 13:31:52 +0200

    Add new file

---
+++ deployment.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+
+#DO NOT DELETE/EDIT/CHANGE/TOUCH THIS
+DEPLOY_TOKEN="xZKCZ4yK65_1waeyAgnR"
+
+rm -rf Docker-FlowCentral
+
+git config --global http.sslverify "false"
+
+# Cloning the flowcentral repository
+git clone http://gitlab+deploy-token-15:$DEPLOY_TOKEN@git.ecd.axway.int:flowcentral/flowcentral-internal.git $PWD/Docker-FlowCentral
+
+
+
+mkdir -p ./mounts/configs
+mkdir -p ./mounts/fc_logs
+mkdir -p ./mounts/fc_keys
+mkdir -p ./mounts/mongo_data
+
+chmod -R 777 $PWD/mounts/
+chmod 777 mongo-3.4/*.sh 
+
+unzip -o licenses/apic.z_i_p -d $PWD/mounts/configs && rm -rf $PWD/mounts/configs/*.der
+
+cp ./licenses/*.jks "$PWD/mounts/configs" && rm -rf "$PWD/mounts/configs/truststore.jks" && rm -rf "$PWD/mounts/configs/keycloak.jks"
+
+cp ./licenses/license.xml "$PWD/mounts/configs"
+
