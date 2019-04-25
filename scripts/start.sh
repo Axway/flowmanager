7e00012f4817bf7fdcebb6406d44623ba121bba0
Costin Sprincenatu <csprincenatu@axway.com>
2019-04-25 14:12:08 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,19 +9,19 @@ else
 	exit 1
 fi
 
-
 set_status() {
   cd /opt/axway/FlowCentral/
   echo "$1" > "/opt/axway/FlowCentral/"$(dirname $(find runtime -name "index.html" | grep "ROOT/login/index.html"))/status.html
 }
 
+  cd /opt/axway/FlowCentral
+  echo "Configuring Product Started"
+  time java -jar opcmd.jar configure -n -s /opt/axway/FlowCentral/conf.properties -env
+  echo "Configuring Product Ended"
 
 tail -F $(find /opt/axway/fc_logs -name "*.log") &
 if [ ! -f /opt/axway/FlowCentral/runtime/initialized ]; then 
   set_status "STARTING"
-  cd /opt/axway/FlowCentral
-  time java -jar opcmd.jar configure -n -s /opt/axway/FlowCentral/conf.properties -env
-  echo "Configuring Product Ended"
   touch ./runtime/initialized
   echo "Flow Central Starting"
   time java -jar opcmd.jar start
