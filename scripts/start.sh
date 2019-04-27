e6f3c9df2efdb22784899184e71e55a41e6021c8
Costin Sprincenatu <csprincenatu@axway.com>
2019-04-27 20:29:59 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,7 +16,7 @@ set_status() {
 
   cd /opt/axway/FlowCentral
   echo "Configuring Product Started"
-  time java -jar opcmd.jar configure -n -s /opt/axway/FlowCentral/conf.properties -env --debug
+  time java -jar opcmd.jar configure -n -s /opt/axway/FlowCentral/conf.properties -env -debug -consolelog
   echo "Configuring Product Ended"
 
 tail -F $(find /opt/axway/fc_logs -name "*.log") &
