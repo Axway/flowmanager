8a97307e51b05dcb23901f53d8db938cc02e3c53
Costin Sprincenatu <csprincenatu@axway.com>
2020-03-31 11:15:26 +0300

    update structure

--- scripts/gen-amplify-certs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-
-openssl genpkey -algorithm RSA -out ./configs/catalog-private-key.pem -pkeyopt rsa_keygen_bits:2048
-openssl rsa -pubout -in ./configs/catalog-private-key.pem -out ./configs/catalog-public-key.pem
