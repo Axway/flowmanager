1c949ace186758546442b6cad2679136b881d83d
Marius Stanca <me@marius.xyz>
2020-09-01 23:39:10 +0300

    First stage in updating deployment flow

---
+++ docker-compose/README.md
@@ -0,0 +1,11 @@
+# docker-compose
+
+Deploy FlowManger using `docker-compose`.
+
+## Requirements
+
+* `Git`
+* `docker >= 19.03.12`
+* `docker-compose >= 1.26.2`
+
+## Usage
---
+++ docker-compose/files/mongo/scripts/mongo-createuser-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo 'Creating application user, pass and db'
+
+mongo ${MONGO_APP_DATABASE} \
+        --host localhost \
+        --port ${MONGO_INTERNAL_PORT} \
+        -u ${MONGO_INITDB_ROOT_USERNAME} \
+        -p ${MONGO_INITDB_ROOT_PASSWORD} \
+        --authenticationDatabase admin \
+        --eval "db.createUser({user: '${MONGO_APP_USER}', pwd: '${MONGO_APP_PASS}', roles:[{role:'dbOwner', db: '${MONGO_APP_DATABASE}'}]});"
