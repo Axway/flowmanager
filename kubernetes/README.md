861e2473f09608703a9f6d0e523267238440d94f
Marius Stanca <me@marius.xyz>
2020-12-17 11:42:11 +0200

    Kubernetes deployment files (#7)
    Kubernetes deployment manifests

---
+++ kubernetes/README.md
@@ -0,0 +1,55 @@
+# Kubernetes
+
+Kubernetes deployment manifests files.
+
+* [Deploy using Kubernetes standard files](standard/)
+* [Deploy using Helm charts](helm/)
+* [Deploy using helper.sh](/)
+
+## Prerequisites
+
+* Kubernetes 1.17+
+* kubectl 1.17+
+* Helm 2.16+ / Helm 3+ (only for Helm deployment)
+
+## How to deploy FlowManager
+
+* Generate certificates and secrets by running `./helper.sh -gc`
+* Editing manually the yaml file for parameters needed or madatory for the customer ([flowmanager.yaml](helm/flowmanager.yaml) and [mongodb.yaml](helm/mongodb.yaml))
+* Install MongoDb using helm by running `./helper.sh -m`
+* Install MongoDb using helm by running `./helper.sh -fm-h`
+
+> For more information about MongoDB with Helm, please access [this link](https://bitnami.com/stack/mongodb/helm).
+
+```shell
+usage: ./helper.sh args
+      [-gc | -gen-certs]
+      [-fm-s | -fm-singlenode]
+      [-fm-m | -fm-multinode]
+      [-fm-h | -flowmanager]
+      [-r | -redis]
+      [-m | -mongodb]
+      [-hi | -history]
+      [-da | -delete-all]
+      [-logs-fm]
+      [-logs-mongodb]
+      [-logs-redis]
+sample:
+ ./helm-helper.sh -gc -fm
+or
+ ./helm-helper.sh -flowmanager
+```
+
+ _Options:_
+
+ - **[-gc | -gen-certs]**       : Generate certificates and create kubernetes secrets.
+ - **[-fm-s | -fm-singlenode]** : Install FlowManager SingleNode using standard K8S. [Change the values here](standard/singlenode/)
+ - **[-fm-m | -fm-multinode]**  : Install FlowManager MultiNode using standard K8S. [Change the values here](standard/multinode/)
+ - **[-fm-h | -fm-flowmanager]**: Install FlowManager MultiNode using Helm. [Change the values here](helm/flowmanager.yaml)
+ - **[-r | -redis]**            : Install Redis using helm.[Change the values here](helm/redis.yaml)
+ - **[-m | -mongodb]**          : Install MongoDb using helm.[Change the values here](helm/mongodb.yaml)
+ - **[-hi | -history]**         : Check Helm logs under namespace.
+ - **[-da | -delete-all]**      : Delete all the resources from namespace.
+ - **[-logs-fm]**               : Check FlowManager logs.
+ - **[-logs-mongodb]**          : Check MongoDb logs.
+ - **[-logs-redis]**            : Check Redis logs.
---
+++ kubernetes/helm/helper_functions/helm_functions.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+# set -x
+###########################################
+# startup function
+###########################################
+function namespace_choice(){
+  echo "Please gives your namespace:" 
+  read -r NAMESPACE 
+  export NAMESPACE=${NAMESPACE}
+  if kubectl get namespace ${NAMESPACE} | grep -q 'Active'
+  then
+    msg_info "The namespace ${NAMESPACE} exists"
+  else
+    msg_info "The namespace ${NAMESPACE} does not exist. Do you want to create it?"
+    select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) kubectl create namespace ${NAMESPACE};msg_info "The namespace ${NAMESPACE} was created"; break;;
+        No ) exit;;
+    esac
+    done
+  fi
+
+}
+
+function startup_script(){
+  purgelog
+  
+  mkdir -p logs
+  
+  exec > >(tee -i ./logs/helm_flowmanager_$(date +"%Y-%m-%d_%H-%M").log)
+
+  namespace_choice
+
+  type -P kubectl &>/dev/null && msg_info 'kubectl is installed.'|| msg_error 'Error: kubectl is not installed.'
+  type -P ${HELM} &>/dev/null && msg_info 'helm is installed.'|| msg_error 'Error: Helm is not installed.'
+
+  msg_output 'Current context set on the namespace flowmanager'
+  kubectl config set-context --current --namespace=${NAMESPACE}
+
+}
+###########################################
+# Purge logs function
+###########################################
+function purgelog() {
+    msg_info 'Purge existing logs older than 10 minutes ago'
+    for deleting in $(find helm_flowmanager_*.log -cmin -10); do
+        msg_output "Log file deleted: $(echo $deleting)"
+        rm -rf $deleting
+    done
+}
+
+###########################################
+# function helm history
+###########################################
+function helm_history() {
+
+    msg_info "Helm Charts History"
+
+    LIST=$($HELM list --namespace ${NAMESPACE} | grep flowmanager | awk '{print $1}')
+    for i in ${LIST}
+    do
+      msg_output "Helm chart ${i}"
+      $HELM history $i -n ${NAMESPACE} 
+    done
+}
+###########################################
+# check stack deletion function
+###########################################
+function stack_confirm_deletion() {
+    msg_info 'Stack deletion step'
+    read -p "Continue (y/n)?" choice
+    case "$choice" in
+    y | Y)
+        exec > >(tee -i ./logs/helm_flowmanager_$(date +"%Y-%m-%d_%H-%M").log)
+        stack_deletion
+        ;;
+    n | N)
+        exit 1
+        ;;
+    *)
+        echo "invalid choice"
+        ;;
+    esac
+}
+
+function stack_deletion() {
+  msg_error 'Starting Stack deletion'
+  
+  kubectl delete pvc -l app=flowmanager-mongodb -n ${NAMESPACE}
+  kubectl delete pvc -l app=flowmanager-redis -n ${NAMESPACE}
+  helm delete flowmanager-redis -n ${NAMESPACE}
+  helm delete flowmanager-mongodb -n ${NAMESPACE}
+  
+  LIST=$($HELM list --namespace ${NAMESPACE} | grep flowmanager | awk '{print $1}')
+  for i in ${LIST}
+  do
+    $HELM delete $i -n ${NAMESPACE} 
+  done
+  
+}
+
+function yaml_parse_file() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+
+  echo ""
+}
+
+function values_file_edit() {
+    local VALUES_FILE=$1
+    local HELM_PACKAGE=$2
+
+    msg_info 'Parameters Validation step'
+    
+    yaml_parse_file ${VALUES_FILE} ${HELM_PACKAGE}
+
+    echo ""
+    read -p "Continue (y/n)?" choice
+    case "$choice" in
+    y | Y)
+        msg_info "Installation/Upgrade on going with this values file: ${VALUES_FILE}"
+        ;;
+    n | N)
+        msg_error "Please update the file ${VALUES_FILE} and relaunch the script"
+        exit 1
+        ;;
+    *)
+        msg_error "invalid choice"
+        exit 1
+        ;;
+    esac
+}
